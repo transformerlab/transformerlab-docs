dd76fa6b6768014158226590ab41dcf7ad61e8cf
ali asaria <aliasaria@users.noreply.github.com>
2025-06-10 12:34:37 -0400

    add diffusion to homepage

--- src/pages/homepage-components/AllFeatures.tsx
+++ src/pages/homepage-components/AllFeatures.tsx
@@ -5,6 +5,7 @@ import FeatureEvals from "./features/feature-evaluate-models.mdx";
 import FeaturePlugins from "./features/feature-plugins.mdx";
 import FeatureRag from "./features/feature-rag.mdx";
 import FeatureHardware from "./features/feature-hardware.mdx";
+import FeatureDiffusion from "./features/feature-diffusion.mdx";
 import FeatureCard from "./features/FeatureCard";
 
 import ModelsImage from "./features/img/models.png";
@@ -14,6 +15,7 @@ import EvalImage from "./features/img/eval.png";
 import PluginsImage from "./features/img/plugins.png";
 import RagImage from "./features/img/rag.png";
 import PlatformsImage from "./features/img/platforms.png";
+import DiffusionImage from "./features/img/diffusion.png";
 
 import React from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
@@ -41,20 +43,26 @@ export default function AllFeatures() {
         text={<FeatureChat />}
         flip
       />
-      <FeatureCard video={<img src={TrainImage} />} text={<FeatureTrain />} />
       <FeatureCard
-        video={<img src={EvalImage} />}
-        text={<FeatureEvals />}
+        video={<img src={DiffusionImage} />}
+        text={<FeatureDiffusion />}
+      />
+      <FeatureCard
+        video={<img src={TrainImage} />}
+        text={<FeatureTrain />}
         flip
       />
+      <FeatureCard video={<img src={EvalImage} />} text={<FeatureEvals />} />
       <FeatureCard
         video={<img src={PluginsImage} />}
         text={<FeaturePlugins />}
+        flip
       />
-      <FeatureCard video={<img src={RagImage} />} text={<FeatureRag />} flip />
+      <FeatureCard video={<img src={RagImage} />} text={<FeatureRag />} />
       <FeatureCard
         video={<img src={PlatformsImage} />}
         text={<FeatureHardware />}
+        flip
       />
     </div>
   );
