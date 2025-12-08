9dfff15bd60ffe2a5824f1d367b0c9028b2906b9
ali asaria <aliasaria@users.noreply.github.com>
2025-12-08 11:20:20 -0500

    better on mobile

--- src/pages/homepage-components/features/FeatureCard.tsx
+++ src/pages/homepage-components/features/FeatureCard.tsx
@@ -4,26 +4,16 @@ import styles from "./features.module.css";
 
 export default function FeatureCard({ video, text, flip = false }) {
   return (
-    <div className={clsx("row", styles.featureCard)} style={{}}>
-      {flip ? (
-        <>
-          <div className={clsx("col col--7", styles.featureCardText)}>
-            {text}
-          </div>
-          <div className={clsx("col col--5", styles.featureCardVideo)}>
-            {video}
-          </div>
-        </>
-      ) : (
-        <>
-          <div className={clsx("col col--5", styles.featureCardVideo)}>
-            {video}
-          </div>
-          <div className={clsx("col col--7", styles.featureCardText)}>
-            {text}
-          </div>
-        </>
-      )}
+    <div
+      className={clsx("row", styles.featureCard)}
+      style={{ flexDirection: flip ? "row-reverse" : "row" }}
+    >
+      <>
+        <div className={clsx("col col--7", styles.featureCardText)}>{text}</div>
+        <div className={clsx("col col--5", styles.featureCardVideo)}>
+          {video}
+        </div>
+      </>
     </div>
   );
 }
